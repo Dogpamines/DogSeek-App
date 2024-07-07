b50c5ed55f4fc80ad5c93f3d4753bba9d5ce7d74
kooyesung <jh8ckr@gmail.com>
2024-07-07 16:59:12 +0900

    [#1] restApi

--- lib/dict.dart
+++ lib/dict.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:dogsick_project/dogDetail.dart';
 import 'package:flutter/material.dart';
-
 import 'location.dart';
 
 class Dict extends StatefulWidget {
@@ -11,15 +11,52 @@ class Dict extends StatefulWidget {
 }
 
 class _DictState extends State<Dict> {
-  final dio = Dio();
+  final Dio dio = Dio();
+  List<dynamic> _dogs = [];
+  List<dynamic> _filteredDogs = [];
+  bool _isLoading = true;
+  String _searchText = "";
 
-  Future<Map<String, dynamic>> fetchDogs() async {
-    final response = await dio.get('http://localhost:8080/dict');
-    if (response.statusCode == 200) {
-      return response.data;
-    } else {
-      throw Exception('Failed to load dogs');
+  @override
+  void initState() {
+    super.initState();
+    _fetchDogs();
+  }
+
+  Future<void> _fetchDogs() async {
+    try {
+      final response = await dio.get('http://10.0.2.2:8080/dict');
+      if (response.statusCode == 200) {
+        setState(() {
+          _dogs = response.data['dict'];
+          _filteredDogs = _dogs;
+          _isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load dogs');
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print(e);
     }
+  }
+
+  void _filterDogs(String query) {
+    setState(() {
+      _searchText = query;
+      if (_searchText.isEmpty) {
+        _filteredDogs = _dogs;
+      } else {
+        _filteredDogs = _dogs.where((dog) {
+          return dog['dogName']
+              .toString()
+              .toLowerCase()
+              .contains(_searchText.toLowerCase());
+        }).toList();
+      }
+    });
   }
 
   @override
@@ -52,7 +89,78 @@ class _DictState extends State<Dict> {
             width: 60,
           )
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(48.0),
+          child: Container(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            margin: EdgeInsets.only(top: 10),
+            child: TextField(
+              onChanged: _filterDogs,
+              decoration: InputDecoration(
+                hintText: '견종을 입력 해주세요!',
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromRGBO(99, 197, 74, 100)),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _filteredDogs.isEmpty
+              ? Center(child: Text('검색 결과가 없어요'))
+              : ListView.builder(
+                  itemCount: _filteredDogs.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: EdgeInsets.only(top: 10),
+                      child: Container(
+                        color: Colors.white,
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                OutlinedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => DogDetail(
+                                          dog: _filteredDogs[index]['dogName']
+                                              .toString(),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    _filteredDogs[index]['dogName'].toString(),
+                                    style: TextStyle(
+                                        fontSize: 14, color: Colors.black),
+                                  ),
+                                  style: OutlinedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    side: BorderSide(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
     );
   }
 }
